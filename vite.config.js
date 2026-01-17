aa16dbfcfcd4e5a8d4ad32d0f6473e4026a7dd2e
nakasho-hub <nakasho0902@gmail.com>
2026-01-17 16:31:56 +0900

    package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ▼ ここを追加（自分のリポジトリ名に変えてください！）
+  base: 'https://github.com/nakasho-hub/island-layout-tool/', 
 })
